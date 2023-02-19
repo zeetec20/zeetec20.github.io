6073cbf008bab2f1a552832ba4c4d49ce6275407
Firman Justisio Lestari <jusles363@gmail.com>
2023-02-19 12:12:20 +0700

    chore: remove unused underline at portfolio page

--- pages/portfolio/[slug].tsx
+++ pages/portfolio/[slug].tsx
@@ -15,12 +15,13 @@ import Link from 'next/link'
 import dotenv from 'dotenv'
 import rehypeRaw from 'rehype-raw'
 import ImageShimmer from '@/component/ImageShimmer';
+import Tag from '@/component/Tag';
 
 const Markdown = (props: { markdown: string }) => {
   return <ReactMarkdown components={{// eslint-disable-next-line react/display-name
-    img: ({node, ...props}) => (
-      <div className='radius img-thumbnail' style={{border: 'calc(1px + 0.25rem) solid #dee2e6', padding: '0'}}>
-        <ImageShimmer quality={90} src={props.src ?? ''} style={{position: 'relative'}} layout="fill" objectFit="contain" alt='' />
+    img: ({ node, ...props }) => (
+      <div className='radius img-thumbnail' style={{ border: 'calc(1px + 0.25rem) solid #dee2e6', padding: '0' }}>
+        <ImageShimmer quality={90} src={props.src ?? ''} style={{ position: 'relative' }} layout="fill" objectFit="contain" alt='' />
       </div>
     ),
     code({ node, inline, className, children, ...props }) {
@@ -41,35 +42,39 @@ const Markdown = (props: { markdown: string }) => {
   >{props.markdown}</ReactMarkdown>
 }
 
-const DetailPortfolioPage = ({ status, meta, article }: {status: any, meta: any, article: any}) => {
+const DetailPortfolioPage = ({ status, meta, article }: { status: any, meta: any, article: any }) => {
   const meta_name = `${meta['title']} | Firman ✋`
   const meta_description = meta['description']
   const meta_image = `${process.env.domain}${meta['thumbnail']}`
   if (status == 404) return <NextError statusCode={status} />
 
-  let tags = (meta['tag'] as string[]).map((tag, key) => <Link href={`/portfolio/tag/${tag.toLowerCase()}`} key={key} passHref><span style={{cursor: 'pointer'}}><ArticleComponent.Tag tag={tag} className={styles.tag} /></span></Link>)
+  let tags = (meta['tag'] as string[]).map((tag, key) => (
+    <Link href={`/portfolio/tag/${tag.toLowerCase()}`} key={key} style={{ textDecoration: 'none' }} passHref>
+      <Tag tag={tag} className={styles.tag} />
+    </Link>
+  ))
 
   return (
     <>
       <Head>
         <title>{meta_name}</title>
 
-        <meta name="description" content={meta_description}/>
+        <meta name="description" content={meta_description} />
 
-        <meta itemProp="name" content={meta_name}/>
-        <meta itemProp="description" content={meta_description}/>
-        <meta itemProp="image" content={meta_image}/>
+        <meta itemProp="name" content={meta_name} />
+        <meta itemProp="description" content={meta_description} />
+        <meta itemProp="image" content={meta_image} />
 
-        <meta property="og:title" content={meta_name}/>
-        <meta property="og:description" content={meta_description}/>
-        <meta property="og:image" content={meta_image}/>
+        <meta property="og:title" content={meta_name} />
+        <meta property="og:description" content={meta_description} />
+        <meta property="og:image" content={meta_image} />
 
-        <meta name="twitter:title" content={meta_name}/>
-        <meta name="twitter:description" content={meta_description}/>
+        <meta name="twitter:title" content={meta_name} />
+        <meta name="twitter:description" content={meta_description} />
         <meta name="twitter:image" content={meta_image}></meta>
       </Head>
 
-      <Container className={`text-center ${styles.article_width} ${styles.wrap_thumbnail}`} style={{ fontFamily: 'Source Sans Pro'}}>
+      <Container className={`text-center ${styles.article_width} ${styles.wrap_thumbnail}`} style={{ fontFamily: 'Source Sans Pro' }}>
         <Container className={`position-relative ${styles.thumbnail}`}>
           <ImageShimmer quality={75} placeholder='blur' src={meta['thumbnail']} className='img-thumbnail border-0' layout='fill' objectFit='cover' alt='thumnail article' />
         </Container>
@@ -79,8 +84,8 @@ const DetailPortfolioPage = ({ status, meta, article }: {status: any, meta: any,
       </Container>
 
       <Container className={`${styles.article_width} ${styles.wrap_title} p-0 pb-1 pe-1 pe-5 ps-5`} style={{ fontFamily: 'Source Sans Pro', marginTop: '6%' }}>
-        <div className={`border p-0 pb-1 pe-1 border-top-0 border-start-0`} style={{ fontFamily: 'Source Sans Pro', marginTop: '6%'}}>
-          <div className="border p-3 pb-1" style={{overflow: 'auto'}}>
+        <div className={`border p-0 pb-1 pe-1 border-top-0 border-start-0`} style={{ fontFamily: 'Source Sans Pro', marginTop: '6%' }}>
+          <div className="border p-3 pb-1" style={{ overflow: 'auto' }}>
             <p>App : &nbsp; <b><a href={meta['app']} target="_blank" rel="noopener noreferrer">{meta['app']}</a></b> </p>
             <p>Tag : &nbsp; {tags}</p>
           </div>
@@ -107,7 +112,7 @@ export async function getStaticPaths() {
   })
 
   return {
-    paths: portfolio.map(portfolio => { return {params: {slug: portfolio.toLowerCase()}}}),
+    paths: portfolio.map(portfolio => { return { params: { slug: portfolio.toLowerCase() } } }),
     fallback: false,
   }
 }
